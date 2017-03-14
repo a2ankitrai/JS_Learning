948c6cbc7e473c07b21980db37e2f6b1d6ee60ee
a2ankitrai <a2.ankitrai@gmail.com>
2017-03-14 18:13:17 +0530

    arrat

---
+++ ds/array.js
@@ -0,0 +1,16 @@
+
+function testEqual(){
+	
+	var a = ["a","b","c"];
+	var b = ["a","b","c"];
+	
+	if(a == b){
+		console.log("equal");
+	}
+	else{
+		console.log("not equal");
+	}
+	
+}
+
+testEqual();
